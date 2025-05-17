8ca58b00c018a87b1aeae36706fe7dacc9671b94
AnDevArteaga <ramosandres873@gmail.com>
2025-05-17 15:51:37 -0500

    Remove logOut ico

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,17 +1,11 @@
 //Possible brand footer
 import { useNavigate } from "react-router-dom";
-import { LogOut } from "lucide-react";
 import logo from "../../assets/img/TASKLYletter.png";
 import logoWhite from "../../assets/img/TASKLYletterWhite.png";
 import DarkModeToggle from "./toggle-dark-mode";
 
 export function Header() {
     const navigate = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/");
-    };
     return (
         <header className="flex items-center justify-between bg-white dark:bg-gray-900 text-blue-600 px-6 py-4 shadow-md border-b border-gray-300 dark:border-gray-900">
             <div
@@ -34,12 +28,6 @@ export function Header() {
                 {/* Toggle dark mode */}
 
                 <DarkModeToggle />
-
-                <LogOut
-                    size={20}
-                    onClick={handleLogout}
-                    className="text-blue-600 hover:text-blue-800 transition-colors flex-shrink-0 cursor-pointer"
-                />
             </div>
         </header>
     );
